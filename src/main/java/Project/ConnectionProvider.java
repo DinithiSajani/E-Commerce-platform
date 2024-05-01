7d658096545510f4c4f494120f3d0b53728e0879
DinithiSajani <dinithisajani@gmail.com>
2024-05-01 23:06:38 +0530

    u

--- src/main/java/Project/ConnectionProvider.java
+++ src/main/java/Project/ConnectionProvider.java
@@ -1,5 +1,24 @@
 package project;
+<<<<<<< HEAD
 
 public class ConnectionProvider {
 
+=======
+import java.sql.*;
+public class ConnectionProvider {
+   public static Connection getCon()
+   {
+	   try
+	   {
+		   Class.forName("com.mysql.cj.jdbc.Driver");
+		   Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/ecommerce", "root", "123456");
+		   return con;
+	   }
+	   catch(Exception e)
+	   {
+		   System.out.print(e);
+		   return null;
+	   }
+   }
+>>>>>>> Dinithi
 }
